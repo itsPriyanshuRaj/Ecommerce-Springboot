56a9e5b46b3e97ea664d2a38dc6b0012ceabaa9b
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-02-16 17:32:12 +0530

    adding backend folder

---
+++ backend/src/main/java/com/priyanshu/EcomProject/model/Product.java
@@ -0,0 +1,17 @@
+package com.priyanshu.EcomProject.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Product {
+    
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean avaibility;
+    private int quantity;
+}
