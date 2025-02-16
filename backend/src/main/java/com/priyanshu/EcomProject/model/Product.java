48058f31a1bb6f7f4f66ddee9f8fd86bf35a74d7
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-02-17 00:06:06 +0530

    Adding all the changes]

--- backend/src/main/java/com/priyanshu/EcomProject/model/Product.java
+++ backend/src/main/java/com/priyanshu/EcomProject/model/Product.java
@@ -3,15 +3,41 @@ package com.priyanshu.EcomProject.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Product {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
     private String brand;
     private BigDecimal price;
     private String category;
+
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-mm-yyyy")
     private Date releaseDate;
-    private boolean avaibility;
-    private int quantity;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
 }
