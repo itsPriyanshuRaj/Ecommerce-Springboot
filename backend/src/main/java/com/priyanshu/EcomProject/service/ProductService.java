76f8eab122236a136d7b1c6e277b389d4d50b66f
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-02-17 00:03:06 +0530

    Adding service moudle

---
+++ backend/src/main/java/com/priyanshu/EcomProject/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.priyanshu.EcomProject.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.priyanshu.EcomProject.model.Product;
+import com.priyanshu.EcomProject.repo.ProductRepo;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProdcuts(){
+        return repo.findAll();
+    }
+
+    public Product getProductbyID(int id) {
+        
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+       
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+	public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+		
+        product.setImageData(imageFile.getBytes());
+        product.setImageType(imageFile.getContentType());
+        product.setImageName(imageFile.getOriginalFilename());
+		return repo.save(product);
+	}
+
+    public void deleteProduct(int id) {
+        
+        repo.deleteById(id);
+    }
+    
+}
